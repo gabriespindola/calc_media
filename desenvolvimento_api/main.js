74127a795a4626a776a015f4852baad4d8e98017
gabr <gabrielmartinsespindola@gmail.com>
2025-03-26 16:19:32 -0300

    exercicio desenvolvimento api

---
+++ desenvolvimento_api/main.js
@@ -0,0 +1,37 @@
+
+//lista de produtos
+const produtos = [
+    { id: 1, nome: 'Produto 1', preco: 10.0 },
+    { id: 2, nome: 'Produto 2', preco: 20.0 },
+    { id: 3, nome: 'Produto 3', preco: 30.0 }
+  ];
+
+//função para adicionar um novo produto à lista
+  function inserirProduto(nome, preco, novoID) {
+
+    for (let i = 0; i < produtos.length; i++){
+      const novoID =produtos[i];
+    }
+  
+    const novoProduto = {
+      id: novoID + 1,
+      nome: nome,
+      preco: preco
+
+    };
+    
+    produtos.push(novoProduto);
+
+    return novoProduto;
+  }
+
+  const produtoInserido = inserirProduto('Produto 4', 40.0);
+  console.log(produtoInserido);  // mostra o novo produto
+     
+
+
+function listarProdutos() {
+    return produtos;
+}
+
+console.log(produtos); 
